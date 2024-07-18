2e56e9fd8e5c71ffd199e3bb497b544cc88d428d
Vanguri Madhu <145244858+VanguriMadhu@users.noreply.github.com>
2024-07-18 19:24:08 +0530

    done all

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.querySelector('.search-bar input');
     const quickLinks = document.querySelectorAll('.quick-links ul li a');
     const menuToggle = document.getElementById('mobile-menu');
-    const navLinks = document.querySelector('.nav-links');
+    const navLinks = document.getElementById('nav-links');
 
     searchInput.addEventListener('input', () => {
         const query = searchInput.value.toLowerCase();
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
         navLinks.classList.toggle('active');
+        menuToggle.classList.toggle('active');
     });
 });
